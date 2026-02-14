455c4934f35a7b45bb04c229fcfd0d550107b7b4
can826 <cancev55@gmail.com>
2026-02-14 19:41:18 +0100

    * testing fpdf for PDF File * create mmethod create_pdf()

--- learnings.md
+++ learnings.md
@@ -1,6 +1,8 @@
 
 Wenn ich ein Parameter in einer Methode verwenden möchte der bereits in der Klasse initialisert wurde,
 dann muss ich den Paramter nicht an die Methode übergeben sondern kann ihn einfach innerhalb der Methode verwenden! 
+* Jede Klasse soll nur **eine Verantwortlichkeit** haben
+
 
 
 # Fragen
@@ -8,3 +10,25 @@ dann muss ich den Paramter nicht an die Methode übergeben sondern kann ihn einf
 - Wenn ich einer Klasse einen Standardwert für ein Attribut geben möchte, schreibe ich es nicht in die init() sondern, 
 liste es darunter asl Attribut auf ?
 - Wie übergebe ich Parameter an eine Methode, wenn der Parameter aus einer anderen Klasse kommt ? -> zahlt() in main.py Line14
+- 
+- An welcher Stelle runde ich den due ab ? Bereits in der Methode, sodass ich im return schon die gerundete Float habe ?
+  oder erst bei der Ausgabe bzw. dem Schreiben in die PDF?
+  - Option 1: Formatierung der Anzeige mit f String -> .2f (2 Float Nachkommastellen). Durch runden in der Ausgabe bleibt die mathematische 
+  Formel unberührt, für interne Berehcnungen ist das besser! 
+  - Option 2: mathematisches runden in der Methode mit round(zahl, 2), kurz vor dem return
+  -> Merke: Trenne Logik und Präsentation. Logik sauber halten für weitere Berechnungen
+
+# FPDF Package
+## Vorgehen
+* PDF erstellen mit: FPDF(orientation='P', unit='pt',format='A4')
+* Erstelle eine Seite mit instanz.add_page()
+* Font einstellen vor Texterstellung mit set_font(family='Arial' ,size=22, style='B')
+* Zelle erstellen. Eine Zelle ist wie ein Rechteck auf der Seite. Darin erstellen wir den Text!
+  * pdf.cell(w=100, h=80, txt='Flatmate Bill', border=0, align='C', ln=1) # Bekommt die größe der Zelle: w und h sind entsprechen der unit die wir in der Klasse definiert haben siehe oben unit='pt'
+  * pdf.cell(w=50, h=30, txt='Rechnung', border=0)
+
+### Wichitige Methoden und Parameter
+* explain where to put the next cell -> ln=1
+* align: Lage innerhalb der Zelle
+* w=0 -> Zelle nimmt die gesamte Länge
+* border=0 ohne rectangle ausdrucken
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from fpdf import FPDF
+
 class Rechnung():
     """
     Klasse enthält die Daten einer Rechnung. Den Betrag und den Zeitraum.
@@ -12,10 +14,8 @@ class Mitbewohner():
         self.anwesenheit_tage = anwesenheit_tage
 
     def zahlt(self,Rechnung:Rechnung, mitbewohner2): # hier nur der Variablen Name -> darin steckt dann eine Mitbewohner() Instanz
-        #cost_per_day = Rechnung.betrag / 31
-        #amount_for_MA = cost_per_day * self.anwesenheit_tage
-        koeffizient = self.anwesenheit_tage / self.anwesenheit_tage + mitbewohner2.anwesenheit_tage # hier dann Zugriff auf ein Attribut der Instanz
-        due = Rechnung.betrag * koeffizient
+        gewicht = self.anwesenheit_tage / (self.anwesenheit_tage + mitbewohner2.anwesenheit_tage) # hier dann Zugriff auf ein Attribut der Instanz
+        due = Rechnung.betrag * gewicht
         return due
 
 
@@ -23,14 +23,48 @@ class PDF_File():
     def __init__(self,filename:str):
         self.filename = filename
 
-    #def generate(self,Rechnung:Rechnung, Mitbewohner:Mitbewohner, Mitbewohner:Mitbewohner):
-        #pass
+    def __str__(self):
+        return f"Name des Dokuments: {self.filename}"
+
+    def create_PDF(self, rechnung,mitbewohner1,mitbewohner2):
+        # create pdf instanz
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()  # Fügt eine Seite hinzu bzw. erstellt di erste Seite
+
+        # Add Text
+        pdf.set_font(family='Arial', size=22, style='B')
+        ## add a cell --> a cell is like a rectangel
+        pdf.cell(w=0, h=80, txt='Flatmate Bill', border=1, align='C',ln=1)  # Bekommt die größe der Zelle: w und h sind entsprechen der unit die wir in der Klasse definiert haben siehe oben unit='pt'
+        pdf.cell(w=200, h=30, txt='Rechnung', border=1)
+        pdf.set_font(family='Arial', size=18, style='I')
+        pdf.cell(w=120, h=30, txt=rechnung.zeitraum, border=1, ln=1)
+        pdf.cell(w=0, h=50, ln=1)
+
+        # Namen der MA
+        pdf.set_font(family='Arial', size=22, style='B')
+        pdf.cell(w=300, h=30, txt='Anwesende Mitbewohner', border=1, ln=1)
+        pdf.set_font(family='Arial', size=18, style='I')
+        ## MA1
+        pdf.cell(w=150, h=30, txt=mitbewohner1.name, border=1)
+        pdf.cell(w=190, h=30, txt=f'{mitbewohner1.zahlt(Rechnung=rechnung, mitbewohner2=mitbewohner2):.2f} EUR', border=1, ln=1)
+        ## MA2
+        pdf.cell(w=150, h=30, txt=mitbewohner2.name, border=1)
+        pdf.cell(w=190, h=30, txt=f'{mitbewohner2.zahlt(Rechnung=rechnung, mitbewohner2=mitbewohner1):.2f} EUR', border=1, ln=1)
+
+        # Ausgabe der PDF
+        ## Wie benennnen und wie directory auswählen?
+        pdf.output(name=f'files/{self.filename}.pdf')
 
 
 
 
-strom = Rechnung(40, 'Januar')
+
+strom = Rechnung(40, 'März')
 Tom = Mitbewohner('Tom', 31)
 Anna = Mitbewohner('Anna', 21)
 
 print(Anna.zahlt(Rechnung=strom, mitbewohner2=Tom))
+print(Tom.zahlt(Rechnung=strom, mitbewohner2=Anna))
+
+pdf = PDF_File(filename='Strom')
+ausgabe = pdf.create_PDF(rechnung=strom,mitbewohner1=Tom, mitbewohner2=Anna)
